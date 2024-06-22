9c6697e29f02d6109e6365ebb446cc33f2143040
RebardedMeep <kennkool99@gmail.com>
2024-06-22 14:16:49 -0400

    Made player script into one script

--- Scripts/player1.gd
+++ Scripts/player1.gd
@@ -6,9 +6,20 @@ const SPEED = 300.0
 const MAX_HEALTH = 100
 
 var health = MAX_HEALTH
+#controls
+var p1_controls: Array = ["p1-left", "p1-right", "p1-up", "p1-down"]
+var p2_controls: Array = ["p2-left", "p2-right", "p2-up", "p2-down"]
+@export var is_p1 = true
+var controls: Array = []
+
+func _ready():
+	if is_p1:
+		controls = p1_controls
+	else:
+		controls = p2_controls
 
 func _physics_process(_delta):
-	var direction = Input.get_vector("p1-left", "p1-right", "p1-up", "p1-down")
+	var direction = Input.get_vector(controls[0], controls[1], controls[2], controls[3])
 	if direction:
 		velocity = direction * SPEED
 	else:
