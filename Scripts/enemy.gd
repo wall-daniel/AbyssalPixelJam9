6e122a216aa1d75a2c7ec00f61667cc7a78c5987
RebardedMeep <kennkool99@gmail.com>
2024-06-22 14:31:27 -0400

    Added stunning and fixes killing lightning

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
 const SPEED = 100
-@export var health = 1
+@export var health = 2
+#stunned
+var is_stunned:bool = false
+@onready var stuntime = $Stuntime
 
 func _physics_process(_delta):
-	var direction = find_closest() - global_position
-	velocity = direction.normalized() * SPEED
-	move_and_slide()
+	if not is_stunned:
+		var direction = find_closest() - global_position
+		velocity = direction.normalized() * SPEED
+		move_and_slide()
 
 func find_closest() -> Vector2:
 	var players = get_tree().get_nodes_in_group("player")
@@ -25,6 +29,11 @@ func hit():
 	health -= 1
 	if health == 0:
 		death()
+	is_stunned = true
+	stuntime.start()
 
 func _on_hurt_box_body_entered(body):
 	body.hit(10)
+
+func _on_stuntime_timeout():
+	is_stunned = false
--- Scripts/lightning.gd
+++ Scripts/lightning.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-
 func _on_body_entered(body):
 	if body.has_method("hit"):
 		body.hit()
+
+func _on_timer_timeout():
+	queue_free()
